3e9b4be93387c97aacb3c10ee55d26fc7db6da8e
inviona <inviajna@gmail.com>
2024-04-10 20:46:32 +0200

    made the page dynamic using javascript and swiperJs and made the page responsible using media querie

---
+++ index.js
@@ -0,0 +1,36 @@
+let swiper = new Swiper(".slide-container", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    loopFillGroupWithBlank: false,
+    centeredSlides: true,
+    fade: true,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1000: {
+            slidesPerView: 4,
+        },
+        1035: {
+            slidesPerView: 5
+        }
+    },
+    slidesPerGroup: 1,
+});
